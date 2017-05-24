a145e62f96fdad8ba8efe5b5b0a59bc0e5a2da2f
cloudoo <leisy@csair.com>
2017-05-24 18:56:58 +0800

    1.domian pachage reforge 2.app

---
+++ huijia-app/src/main/java/com/cloudo/hj/dao/CoacherInfoDao.java
@@ -0,0 +1,9 @@
+package com.cloudo.hj.dao;
+
+/**
+ * Created by cloudoo on 2017/5/20.
+ */
+public interface CoacherInfoDao {
+
+
+}
--- huijia-domain/src/main/java/com/cloudo/hj/domain/CommonCourseInfo.java
+++ huijia-domain/src/main/java/com/cloudo/hj/domain/CommonCourseInfo.java
@@ -1,4 +1,4 @@
-package domain;
+package com.cloudo.hj.domain;
 
 import java.io.Serializable;
 import java.util.Date;

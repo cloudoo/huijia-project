5b2f3317d7bc2795560fcde7eb02466c77132005
cloudoo <leisongyun@gmail.com>
2017-05-30 23:24:38 +0800

    1.init sql edit

--- doc/init_table.sql
+++ doc/init_table.sql
@@ -1,17 +1,35 @@
+CREATE TABLE `hj_course_info` (
+  `id` bigint(7) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `type` tinyint(3) NOT NULL,
+  `cover_dir` varchar(100) NOT NULL,
+  `duration` int(11) NOT NULL,
+  `min` int(11) NOT NULL DEFAULT '1',
+  `max` int(11) NOT NULL DEFAULT '1',
+  `normal_cost` double NOT NULL,
+  `cost_desc` varchar(200) DEFAULT NULL,
+  `course_desc` varchar(2000) NOT NULL,
+  `valid` tinyint(4) NOT NULL,
+  `op_tm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
 
 drop table hj_course_info;
 
 CREATE TABLE `hj_course_info` (
   `id` bigint(7) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45)  NOT NULL,
+  `name` varchar(45) NOT NULL,
   `type` tinyint(3) NOT NULL,
   `cover_dir` varchar(100) NOT NULL,
   `duration` int(11) NOT NULL,
-  `min` int(11) NOT NULL,
-  `max` int(11) NOT NULL,
+  `min` int(11) NOT NULL DEFAULT '1',
+  `max` int(11) NOT NULL DEFAULT '1',
   `normal_cost` double NOT NULL,
-  `cost_desc` varchar(200)  DEFAULT NULL,
-  `course_desc` varchar(2000)  NOT NULL,
+  `cost_desc` varchar(200) DEFAULT NULL,
+  `course_desc` varchar(2000) NOT NULL,
+  `valid` tinyint(4) NOT NULL,
+  `op_tm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
